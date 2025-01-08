a9e17683b49636e35fcc4ada4762c314b2ca0028
Nemi100 <teksity@gmail.com>
2025-01-08 12:16:57 +0000

    subscriptions completed awaiting stripe integration

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 def subscription_plans(request):
     plans = SubscriptionPlan.objects.all()
     basket_count = SubscriptionBasket.objects.filter(user=request.user).first().plans.count() if request.user.is_authenticated else 0
-    return render(request, 'subscriptions/subscription-plans.html', {'plans': plans, 'basket_count': basket_count})
+    return render(request, 'subscriptions/subscription_plans.html', {'plans': plans, 'basket_count': basket_count})
 
 def add_to_basket(request):
     if request.method == 'POST':
@@ -21,8 +21,38 @@ def add_to_basket(request):
                     request.session['subscription_basket'] = []
                 request.session['subscription_basket'].append(plan.id)
                 request.session.modified = True
-            return redirect('view_basket')
-    return redirect('subscription_plans')
+            return redirect('subscriptions:subscription_basket')
+    return redirect('subscriptions:subscription_plans')
+
+def proceed_to_subscribe(request):
+    if request.method == 'POST':
+        subscription_data = []
+        for key, value in request.POST.items():
+            if key.startswith('subscription_type_'):
+                plan_id = key.split('_')[2]
+                newsletter = request.POST.get(f'newsletter_{plan_id}', 'no')
+                subscription_data.append({
+                    'plan_id': plan_id,
+                    'subscription_type': value,
+                    'newsletter': newsletter
+                })
+        request.session['subscription_data'] = subscription_data
+        return redirect('subscriptions:subscribe')
+    return redirect('subscriptions:subscription_plans')
+
+def switch_plan(request):
+    if request.method == 'POST':
+        plan_id = request.POST.get('plan_id')
+        if request.user.is_authenticated:
+            basket = SubscriptionBasket.objects.get(user=request.user)
+            basket.plans.remove(plan_id)
+        else:
+            subscription_basket = request.session.get('subscription_basket', [])
+            if plan_id in subscription_basket:
+                subscription_basket.remove(plan_id)
+            request.session['subscription_basket'] = subscription_basket
+        return redirect('subscriptions:subscription_plans')
+    return redirect('subscriptions:subscription_plans')
 
 def view_basket(request):
     if request.user.is_authenticated:
@@ -36,21 +66,15 @@ def view_basket(request):
 
 @login_required
 def subscribe(request):
-    plan_id = request.GET.get('plan_id')
-    plan = get_object_or_404(SubscriptionPlan, id=plan_id)
-    
+    subscription_data = request.session.get('subscription_data', [])
+    plans = [get_object_or_404(SubscriptionPlan, id=data['plan_id']) for data in subscription_data]
+    # Here, to handle the payment process or integrate Stripe
+    # For now,i am simulating the payment processing
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-        if form.is_valid():
-            subscription = form.save(commit=False)
-            subscription.user = request.user
-            subscription.plan = plan
-            subscription.save()
-            return redirect('thank_you')
-    else:
-        form = SubscriptionForm(initial={'plan': plan})
-    
-    return render(request, 'subscriptions/subscribe.html', {'form': form, 'plan': plan, 'basket_count': basket_count})
+        # After payment is successful
+        return redirect('subscriptions:thank_you')
+    return render(request, 'subscriptions/subscribe.html', {'plans': plans, 'subscription_data': subscription_data})
 
 def thank_you(request):
-    return render(request, 'subscriptions/thank_you.html')
+    show_toast = True  
+    return render(request, 'subscriptions/thank_you.html', {'show_toast': show_toast})
