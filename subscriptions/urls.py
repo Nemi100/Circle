b632655d1ed16085978ba6a662682d0f9b29f8e8
Nemi100 <teksity@gmail.com>
2025-01-08 12:16:57 +0000

    subscriptions completed awaiting stripe integration

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'subscriptions'  
+app_name = 'subscriptions'
 
 urlpatterns = [
-    path('subscription-plans/', views.subscription_plans, name='subscription_plans'),
+    path('subscription_plans/', views.subscription_plans, name='subscription_plans'),
     path('add_to_basket/', views.add_to_basket, name='add_to_basket'),
-    path('subscription_basket/', views.view_basket, name='view_basket'),
+    path('subscription_basket/', views.view_basket, name='subscription_basket'),
     path('subscribe/', views.subscribe, name='subscribe'),
-    path('thank-you/', views.thank_you, name='thank_you'),
+    path('proceed_to_subscribe/', views.proceed_to_subscribe, name='proceed_to_subscribe'),
+    path('switch_plan/', views.switch_plan, name='switch_plan'),
+    path('thank_you/', views.thank_you, name='thank_you'),
 ]
