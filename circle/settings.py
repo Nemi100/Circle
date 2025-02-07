811403b38717fff4fde3741b5aa929ef39f5cd6b
Nemi100 <teksity@gmail.com>
2025-02-07 10:47:30 +0000

    Update settings for S3 storage

--- circle/settings.py
+++ circle/settings.py
@@ -154,7 +154,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
@@ -162,6 +161,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    
     AWS_STORAGE_BUCKET_NAME = 'circle360tex'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
