d9ed53dfe5e8d0e3c2f2a3344f65ae99826b7704
Nemi100 <teksity@gmail.com>
2025-02-07 11:09:21 +0000

    edit STATIC_ROOT setting for collectstatic

--- circle/settings.py
+++ circle/settings.py
@@ -149,19 +149,20 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
-    
+
     AWS_STORAGE_BUCKET_NAME = 'circle360tex'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
