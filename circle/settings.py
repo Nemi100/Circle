bbd9d076b7b1747579e8175e4995f910c56397fc
Nemi100 <teksity@gmail.com>
2025-05-09 11:14:43 +0100

    Updated Allauth settings to latest format

--- circle/settings.py
+++ circle/settings.py
@@ -108,11 +108,10 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 
 
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
-ACCOUNT_USERNAME_MIN_LENGTH = 4
+# Updated Allauth settings
+ACCOUNT_LOGIN_METHODS = {'email', 'username'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'email2*', 'username*', 'password1*', 'password2*']
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
